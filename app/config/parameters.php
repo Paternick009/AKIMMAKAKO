160e91a6ffdeab2d11f20c67260b90a77c9315e0
Paternick <mipaternick3@gmail.com>
2024-07-19 17:13:43 +0000

    app

---
+++ app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'maboutique',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '76urQ1Ps2UQWiOuRHMtHEW9ZAWZvDpaSHD7U0rbkuz8nS7IGZ04IQrhZZUXB6HiR',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-07-08',
+    'locale' => 'fr-FR',
+    'use_debug_toolbar' => true,
+    'cookie_key' => '00MLDTTctX98oFOqINOp2OLUAY7SNeafy3cKM7GmpifxuM9V7piziRhxBJy394VH',
+    'cookie_iv' => 't8qIzmrWsryQUNhTVyLqoGqV8umYt8HO',
+    'new_cookie_key' => 'def0000046c9300696e04c9e0cc1c68b546cdef71cefd1bf332cf17275a4113d13d87feca244d2e89f44a74df800e11250015e8f981e0c30ca05b3ff7df691b8705c3a78',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz14d6pJFPy6DREt+tzU0
+EHP4czho6KuujnQsJi19GyH6tbw3LEumrWu00FZcHgjTzRUZVvJ9v3ZuB6ZUQ/IF
+vkPd4TMm/Y5jgJy7YuRWRmTcdFZS2ZpDKUQsDjoQ+Q/y8Q5da/EAP7b5+OCdPoVE
+no2i4CcntLB5xTWprhHX64QabCxolAWBypPSYCO2cQn8dKPVwIJfj5yltUs7RwFb
+Jx9CvX4ZBaqWFhm6FbS5GnsnZF+OayuE2hawcDnrXIQVtCB1BlWpAhKyZyom1RgC
+QG3ZXR+np2WQQ8tz528QZuNUJrbBBYH78et8RhS1faHWTKMQIoIxfSSsT4Hp0NWc
+oQIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQDPXh3qkkU/LoNE
+S363NTQQc/hzOGjoq66OdCwmLX0bIfq1vDcsS6ata7TQVlweCNPNFRlW8n2/dm4H
+plRD8gW+Q93hMyb9jmOAnLti5FZGZNx0VlLZmkMpRCwOOhD5D/LxDl1r8QA/tvn4
+4J0+hUSejaLgJye0sHnFNamuEdfrhBpsLGiUBYHKk9JgI7ZxCfx0o9XAgl+PnKW1
+SztHAVsnH0K9fhkFqpYWGboVtLkaeydkX45rK4TaFrBwOetchBW0IHUGVakCErJn
+KibVGAJAbdldH6enZZBDy3PnbxBm41QmtsEFgfvx63xGFLV9odZMoxAigjF9JKxP
+genQ1ZyhAgMBAAECggEAHTYU4MZaGtxigWxhHU3RV7/+b7yyipQkssyKq5Yc2YWf
+25n367V8dVGG1xJTR6fWrj0apLTkIeR8+3bix+9//51v1BlwlKdKPQPXIccooCH1
+LC483Ypti5Ke4iIr4entPyfW4S3B6zfLXtPFUtlTIUKyfwEFBt8VSEKW9H5u/C4O
+pauVKZ/MaIhtNRw8KGW2CsBZAkd7HYgTU9qaRt6CmJ+R+dahp/35AtS7XXFHz0ck
+DXQQzLMAEkdwQoqc/E6zHRi+OyqAwB8fcfZGz7ylMbMRA6FG530nlmEFHmtNkmkh
+L7dzP6rV+unc0FlTcSg7GzIC+L231ViPhqE0zIiMAQKBgQD6sh6J6Bg1S1RwWBYv
+MWo88Oa9h83PbcpWsdQy+wDTjqvMkHeHCG8BW52czw2OSRcs6W9SIxH3SDG/Iqd1
+D5DbjPrk4j92A/SqDejEETLLDlV+xk+AILLrMBbHE6rWKfQCegx0TfbLamE1Febg
+jrzJuG3grDlyQdEMomrGhKtAaQKBgQDTwVAe3CZdkb9yEnBut87SItcqyHEyYLwZ
+4rDDCvtjBDQqfYkiqkalH0LuL6zKFNutmOIWGr1bu2Q06I57bsn7RItLQ1hrfQL7
+AiL5AQbMUOxa2Ntl11V/tdoRjoqpOE/wtUyaKZ7/kNyWUp2IVJ24oSd7edB+0GPn
+kuSvj61zeQKBgHixyIIce4ss+TodLpEJ+VvdnzkgaLW05fVXKBJjRcKUraGO6/zW
+0pqUwQz9okkeZ0n4GOAygjPLTif3RyfUlqkc9q27P1JSFTjJ+YZrwUiBXifq0dud
+4oXYYR73GQiCqjeHCvfzkR3d2W71XHJARiPX5w6D3CRmC39Jsp5gwRIBAoGAcDZL
+p97PsdvvUJq22+Upgy+VSAWp/qFGbI01UFY8eu8aWb3mcuk6wHu9cwzuirPc6bRP
+cpk+6SJ3apYmEwDVpgJfskWA0I0RjERIlh1WJeVoKogqU2B13VRXt+AKWLwPmKXq
+bvNJX3K7gilLc5grcFBt5L/bnaf+mRO9fc8sgjkCfz2dLlh2pdT+BRnf3FfQAgG5
+VfYKiaBTBypFxg3Kv0rdO5mcyU5uBSw28Sl0SPKKPIwdfX9frRpeKDN3DeCivjd3
+CwGehR5xkhxEsY/YKEhaB0AKcwTQBKYPKaExcPdz6Yl8BRn+6f4ilZxaVOM5e5dN
+lktExL4bbOigEgPKxFg=
+-----END PRIVATE KEY-----
+',
+  ),
+);
